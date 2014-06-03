949b1cf59f51e0390ac4609077114d52be49994a
Tim Lucas <t@toolmantim.com>
2014-06-04 04:22:46 +1000

    Move the static content into Rails views

---
+++ app/controllers/destinations_controller.rb
@@ -0,0 +1,17 @@
+class DestinationsController < ApplicationController
+  def index
+    render :index
+  end
+
+  def amsterdam
+    render :amsterdam
+  end
+
+  def provence
+    render :provence
+  end
+
+  def tasmania
+    render :tasmania
+  end
+end
